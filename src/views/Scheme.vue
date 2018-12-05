b277c84bb4c6b78d5ea495e660ea648a485a8871
Askar <aleemov@gmail.com>
2018-12-06 00:26:35 +0600

    Прописываю компоненты для презы

---
+++ src/views/Scheme.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scheme">
+    <img src="../assets/lifecycle.png">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.scheme {
+  text-align: center;
+
+  img {
+    width: 100%;
+    max-width: 600px;
+  }
+}
+</style>
