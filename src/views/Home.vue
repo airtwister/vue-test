e608d78461407455d50db3264138409504fcab83
Askar <aleemov@gmail.com>
2018-12-06 08:14:46 +0600

    Прописываю компоненты для презы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/scheme">Схема</router-link>
+      <router-link to="/v-if">v-if и computed</router-link>
+      <router-link to="/v-model">v-model</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+  #nav {
+    padding: 30px;
+
+    a {
+      display: block;
+      font-weight: bold;
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
